eb057165fb0b1ef0cb82da8b24deb38e5cf7f58a
Akash <akashdhonikrishnan@gmail.com>
2026-01-25 13:30:46 +0530

    Updated Upto Dec 2025
    Signed-off-by: Akash <akashdhonikrishnan@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
 function openIframe(src) {
-    document.getElementById('iframe').src = src;
-    document.getElementById('iframe').style.display = 'block';
+    document.getElementById('iframe').style.display = 'none';
+    document.getElementById('iframe2').style.display = 'none';
+    document.getElementById('iframe3').style.display = 'none';
+
+    if (src === 'About.html') {
+        document.getElementById('iframe').style.display = 'block';
+    } else if (src === 'resume.html') {
+        document.getElementById('iframe2').style.display = 'block';
+    } else if (src === 'projects.html') {
+        document.getElementById('iframe3').style.display = 'block';
+    }
 }
+
 const activeElements = document.querySelectorAll('.active-on-click');
 
 activeElements.forEach(element => {
   element.addEventListener('click', () => {
     activeElements.forEach(activeElement => {
-      activeElement.classList.remove('.active-element');
+      activeElement.classList.remove('active-element');
     });
-    element.classList.add('.active-element');
+    element.classList.add('active-element');
   });
 });
